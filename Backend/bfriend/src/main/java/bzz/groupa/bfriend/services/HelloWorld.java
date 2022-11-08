34b900eeacdf406f1d117b9c581d6bda160f99ab
Zwazel <zwazel@hotmail.com>
2022-11-08 16:32:55 +0100

    WIP 3

--- Backend/bfriend/src/main/java/bzz/groupa/bfriend/services/HelloWorld.java
+++ Backend/bfriend/src/main/java/bzz/groupa/bfriend/services/HelloWorld.java
@@ -1,11 +1,13 @@
 package bzz.groupa.bfriend.services;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloWorld {
-    @GetMapping(value = "/")
+    @GetMapping(path = "/hello", produces = "text/plain")
     public String helloWorld() {
         return "Hello World!";
     }
