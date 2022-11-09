e2715edf0c15d468850770bfeed1fa7c2aa42f07
Zwazel <zwazel@hotmail.com>
2022-11-09 16:11:27 +0100

    WIP

---
+++ Backend/bfriend/src/main/java/bzz/groupa/bfriend/services/AuthController.java
@@ -0,0 +1,10 @@
+package bzz.groupa.bfriend.services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/auth")
+@Controller
+public class AuthController {
+
+}
