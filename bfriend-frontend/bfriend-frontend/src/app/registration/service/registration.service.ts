a9d9437b45e0ca702b1f2792e3a7c42d3c60935f
Surber Fabian <fabian.surber@bbcag.ch>
2022-11-22 09:21:43 +0100

    angular material

--- bfriend-frontend/bfriend-frontend/src/app/registration/service/registration.service.ts
+++ bfriend-frontend/bfriend-frontend/src/app/registration/service/registration.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  private sendData(user: User) {
+    this.http.post<User>('backendURL', user).subscribe(data => {})
+  }
 }
