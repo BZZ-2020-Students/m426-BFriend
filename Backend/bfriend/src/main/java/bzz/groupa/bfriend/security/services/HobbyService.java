958ee9d9f297431408b934026db17ca4aab8e323
Zwazel <zwazel@hotmail.com>
2022-11-22 11:43:39 +0100

    service to get all hobbies

---
+++ Backend/bfriend/src/main/java/bzz/groupa/bfriend/security/services/HobbyService.java
@@ -0,0 +1,25 @@
+package bzz.groupa.bfriend.security.services;
+
+import bzz.groupa.bfriend.enums.Hobby;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@RequestMapping("/api/hobby")
+@Controller
+public class HobbyService {
+    @GetMapping(path = "/all", produces = "application/json")
+    public ResponseEntity<?> getAllHobbies() {
+        var hobbies = Hobby.values();
+
+        var hobbiesList = new ArrayList<String>();
+        for (Hobby hobby : hobbies) {
+            hobbiesList.add(hobby.getNiceName());
+        }
+
+        return ResponseEntity.ok().body(hobbiesList);
+    }
+}
