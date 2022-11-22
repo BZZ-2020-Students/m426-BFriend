bd57dc25435d8ebcdf730684c5e573545b77cce3
Gayathiri01 <76039864+Gayathiri01@users.noreply.github.com>
2022-11-22 11:23:49 +0100

    first commit

--- bfriend-frontend/bfriend-frontend/src/app/app.module.ts
+++ bfriend-frontend/bfriend-frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+  ]
 })
 export class AppModule { }
---
+++ bfriend-frontend/bfriend-frontend/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+
+})
+export class HomeComponent implements OnInit {
+  //authForm: FormControl = new FormControl('');
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
