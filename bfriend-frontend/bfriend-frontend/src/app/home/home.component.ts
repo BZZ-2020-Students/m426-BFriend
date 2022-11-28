310b21958509fd5a0597546d22a61cf46bacaa1c
Gayathiri01 <76039864+Gayathiri01@users.noreply.github.com>
2022-11-28 20:06:59 +0100

    added login backend

--- bfriend-frontend/bfriend-frontend/src/app/home/home.component.ts
+++ bfriend-frontend/bfriend-frontend/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import { FormGroup,FormControl, Validators } from '@angular/forms';
 import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material'
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,18 +14,47 @@ import {MatDialog} from '@angular/material'
 export class HomeComponent implements OnInit {
   //authForm: FormControl = new FormControl('');
   //loginForm: FormGroup = new FormGroup('');
-    email: FormControl = new FormControl('', [Validators.required, Validators.email]);
-    password: FormControl = new FormControl('', [Validators.required]);
+    //email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+    //password: FormControl = new FormControl('', [Validators.required]);
+  login:FormGroup|any;
 
 
-  constructor() { }
+  constructor(private _http:HttpClient, private _route:Router) { }
 
 
   ngOnInit(): void {
+    this.login = new FormGroup({
+      'email': new FormControl(),
+      'password': new FormControl()
+    })
 
   }
 
   onSubmit() {
+
+  }
+
+  logindata(login:FormGroup){
+    this._http.get<any>("http://localhost:8080/")
+      .subscribe(res=>{
+        const user = res.find((a:any)=>{
+          return a.email === this.login.value.email && a.password === this.login.value.password
+        });
+
+        if(user){
+          alert('you are successfully login');
+          this.login.reset();
+          $('.form-box').css('display','none');
+          this._route.navigate(['api/auth/login']);
+        }else{
+          alert('User Not Found');
+          this._route.navigate(['home']);
+        }
+
+      }, err=>{
+        alert('Something was wrong');
+      })
+
 
   }
 
