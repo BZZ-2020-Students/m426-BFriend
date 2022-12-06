f86cce9b807af95fbc95c3e9769e1b3525fdf618
Tim Irmler <zwazel@hotmail.com>
2022-12-06 23:31:45 +0100

    not so pretty search bar lol

--- bfriend-frontend/bfriend-frontend/src/app/registration/registration.component.ts
+++ bfriend-frontend/bfriend-frontend/src/app/registration/registration.component.ts
@@ -5,6 +5,7 @@ import {MyLocation, RegistrationService} from "./service/registration.service";
 interface DropDownItem {
   item_id: string;
   item_text: string;
+  combination: string;
 }
 
 @Component({
@@ -22,15 +23,15 @@ export class RegistrationComponent implements OnInit {
       gender: new FormControl(''),
       age: new FormControl(''),
       password: new FormControl(''),
+      locationSearch: new FormControl(''),
     }
   );
   @ViewChild('hobby_selection') hobbySelection: any;
   selectedHobbies = [];
   dropdownSettingsHobbies = {};
 
-  @ViewChild('location_selection') locationSelection: any;
-  selectedLocation = [];
-  dropdownSettingsLocation = {};
+  selectedLocation: DropDownItem[] = [];
+  foundLocations: DropDownItem[] = [];
   locationQueryInterval: any;
 
   constructor(private registrationService: RegistrationService) {
@@ -60,35 +61,32 @@ export class RegistrationComponent implements OnInit {
       noDataAvailablePlaceholderText: 'No Data Available',
       allowRemoteDataSearch: false,
     }
-
-    this.dropdownSettingsLocation = {
-      singleSelection: true,
-      idField: 'item_id',
-      textField: 'item_text',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      allowSearchFilter: true,
-      searchPlaceholderText: 'Search for location',
-      noDataAvailablePlaceholderText: 'No Data Available',
-      allowRemoteDataSearch: true,
-    }
   }
 
   getNewLocationData(event: any) {
     clearTimeout(this.locationQueryInterval);
     if (event.length > 0) {
       this.locationQueryInterval = setTimeout(() => {
-        console.log("lets go", event)
         this.registrationService.getLocations(event).subscribe({
           next: data => {
             let locations: DropDownItem[] = [];
             data.data.forEach((location: MyLocation) => {
-              locations.push({item_id: location.wikiDataId, item_text: location.name});
+              let location_item = {
+                item_id: location.wikiDataId,
+                item_text: location.name,
+                combination: `${location.wikiDataId};${location.name}`
+              };
+              locations.push(location_item);
             });
-            this.locationSelection.data = locations;
+
+            this.foundLocations = locations;
           }
         });
       }, 500);
     }
+  }
+
+  onLocationSelect(event: any) {
+    console.log(event);
   }
 }
